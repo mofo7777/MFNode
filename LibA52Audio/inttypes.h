357c2ab4dab7f6c3c41318a13274621351786e1f
mofo7777 <41967574+mofo7777@users.noreply.github.com>
2018-09-09 18:51:54 +0200

    Add files via upload

---
+++ LibA52Audio/inttypes.h
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------------------------------
+// inttypes.h
+//----------------------------------------------------------------------------------------------
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#endif
